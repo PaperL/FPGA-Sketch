9f0dd73b48863714c828079e456c9a6835aa5104
FrankQiu <frankq1024@gmail.com>
2024-05-05 15:39:18 +0800

    Remove local files

--- local_sim.sh
+++
@@ -1,5 +0,0 @@
-conda activate nf
-source /tools/Xilinx/Vivado/2020.2/settings64.sh
-source ./tools/settings.sh
-cd $NFPLUS_FOLDER
-./tools/scripts/nf_test.py sim --major loopback --minor minsize --dump
--- local_sim_fix.sh
+++
@@ -1,5 +0,0 @@
-conda activate nff
-source /tools/Xilinx/Vivado/2021.1/settings64.sh
-source ./tools/settings.sh
-cd $NFPLUS_FOLDER
-./tools/scripts/nf_test.py sim --major few --minor minsize
